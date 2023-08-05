04a53931d3c1811e5523d3fcd200ebaf8f702cea
nicolasjcortez <jordaocortez@gmail.com>
2023-08-05 17:16:00 -0300

    check tests before deploy

--- deployHeroku.ps1
+++ deployHeroku.ps1
@@ -1,3 +1,16 @@
+# Run tests using `go test` and capture the output
+$testResult = Invoke-Expression "go test ./... 2>&1"
+
+# Check if the tests were successful
+if ($testResult -match "FAIL") {
+    Write-Host "Tests failed, deployment aborted."
+    Write-Host $testResult   # Output the test results
+    exit 1
+} else {
+    Write-Host "Tests passed, proceeding with local execution..."
+}
+
+
 # heroku authorizations:create
 # $env:HEROKU_API_KEY = "#####"; 
 docker login --username=_ --password=$(heroku auth:token) registry.heroku.com
